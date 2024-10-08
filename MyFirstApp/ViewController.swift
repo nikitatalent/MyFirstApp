5c95ebfe74dba1b77f2403c802e5b5f61e081008
Nikita <nikitatalent@gmail.com>
2024-10-08 18:09:33 +0300

    готовый проект

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var StartButton: UIButton!
+    @IBOutlet weak var mainText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("Кнопка успешно нажата 😁")
+        
+        view.backgroundColor = .systemBackground
+        
+        mainText.text = "Дорогие друзья 😁"
+        mainText.textColor = .label
+        mainText.textAlignment = .center
+        
+        StartButton.setTitle("Получилось!", for: .normal)
+        
+    }
+    
 }
 
